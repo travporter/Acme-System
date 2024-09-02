557090d715582dae3dc7242058d19e09d8c1a49f
Travis Porter <portertravis404@gmail.com>
2024-09-02 19:56:56 -0400

    Added heyNow method

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -7,5 +7,10 @@ public class HelloWorld {
 		System.out.println("Hello from Eclipse");
 		System.out.println("Dave is working locally on this branch");
 	}
+	
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
 
 }
